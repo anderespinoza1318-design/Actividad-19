8bba720a3fd7ad2b81d589f0b9b2f67ffe0ed424
cristian0908car-netizen <cristian.0908.car@gmail.com>
2026-05-12 14:21:30 -0500

    print de saludo al usuario, primer linea

--- Actividad.19.py
+++ Actividad.19.py
@@ -1,53 +1,48 @@
-print ("Hola Bienvenido amigo")
-print ("Este menú tiene 3 opciones y son las siguientes:")
-print ("1. Consultar su indice de masa corporal (MIC) ")
-print ("2. Operaciones Aritmeticas")
-print ("3. Salir")
-opcion = input("Digita la opcion aqui: ")
+print("Hola")
 
-if opcion == "1":
-     print ("HOLA ")
-     nombre = input ("Ingrese su nombre: ")
-     edad = input ("Ingrese su edad: ")
-     peso = float(input ("Ingrese su peso: "))
-     altura = float(input ("Ingrese su altura: "))
-     alturacuadrado = (altura**2)
-     print ("Bien ", nombre + " su Edad es ",  edad, "Años" " y su IMC es de ",  (peso/alturacuadrado))
-     salir1 = input ("Si deseas volver pulse 3: ")
-     while salir1 == "3":
-          print ("1. Consultar su indice de masa corporal (MIC) ")
-          print ("2. Operaciones Aritmeticas")
-          print ("3. Salir") 
-          opcion = input("Digita la opcion aqui: ")
-          break
+opcion = "0"
 
-elif opcion == "2":
-     print ("Aqui salen todas las Operaciones Aritmeticas")
-     num1 = int(input (" Ingrese un primer numero: "))
-     num2 = int(input ("Ingrese el segundo numero: ")) 
-     suma = (num1+num2)
-     resta = (num1-num2)
-     multiplicación = (num1*num2)
-     division = (num1/num2)
-     divisionE = (num1//num2)
-     potenciacion = ( num1**num2)
-     print ("su suma es: ", suma) 
-     print ("su resta es: ", resta)
-     print ("su multiplicacion es: ", multiplicación)  
-     print ("Su Division es: ", division)
-     print ("Su Division Entera es: ", divisionE)
-     print ("Su Potenciacion es: ", potenciacion)
-     salir2 = input("Si deseas volver digite 3: ")
-     while salir2 == "3":
-            print ("1. Consultar su indice de masa corporal (MIC) ")
-            print ("2. Operaciones Aritmeticas")
-            print ("3. Salir") 
-            opcion = input("Digita la opcion aqui: ")
-            break
-          
+while opcion != "3":
+    print("\nzBienvenido al menu principal")
+    print("1. Consultar su indice de masa corporal (IMC)")
+    print("2. Operaciones Aritmeticas")
+    print("3. Salir")
+    opcion = input("Digita la opcion aqui: ")
 
-else:
-      print("Saliendo")
+    if opcion == "1":
+        nombre = input("Ingrese su nombre: ")
+        edad = input("Ingrese su edad: ")
+        peso = float(input("Ingrese su peso (kg): "))
+        altura = float(input("Ingrese su altura (m): "))
+        alturacuadrado = altura ** 2
+        imc = peso / alturacuadrado
+        print("Bien", nombre, "su edad es de", edad, "anos y su IMC es de", imc)
+        volver = input("Deseas volver al menu? Pulsa 3, sino pulsa cualquier tecla: ")
+        if volver != "3":
+            opcion = "3"
 
+    elif opcion == "2":
+        print("Aqui salen todas las Operaciones Aritmeticas")
+        num1 = int(input("Ingrese un primer numero: "))
+        num2 = int(input("Ingrese el segundo numero: "))
+        suma = num1 + num2
+        resta = num1 - num2
+        multiplicacion = num1 * num2
+        division = num1 / num2
+        divisionE = num1 // num2
+        potenciacion = num1 ** num2
+        print("Su suma es:", suma)
+        print("Su resta es:", resta)
+        print("Su multiplicacion es:", multiplicacion)
+        print("Su division es:", division)
+        print("Su division entera es:", divisionE)
+        print("Su potenciacion es:", potenciacion)
+        volver = input("Deseas volver al menu? Pulsa 3, sino pulsa cualquier tecla: ")
+        if volver != "3":
+            opcion = "3"
 
-     
+    elif opcion == "3":
+        print("Saliendo... Gracias por usar!")
+
+    else:
+        print("Opcion no valida. Elige 1, 2 o 3.")
